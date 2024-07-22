46ffd169e1648e9838aebf5b69976cc60a7f1dcb
non_hana <1209220829@qq.com>
2024-07-22 10:43:22 +0800

    feat: 更新证书位置

--- bin/server.ts
+++ bin/server.ts
@@ -28,10 +28,10 @@ function createServer() {
   if (process.env.NODE_ENV === "production") {
     const options = {
       key: fs.readFileSync(
-        path.join(__dirname, "../public/ssl/nonhana-server.one.key")
+        path.join(__dirname, "../public/ssl/nonhana-server.cn.key")
       ),
       cert: fs.readFileSync(
-        path.join(__dirname, "../public/ssl/nonhana-server.one.pem")
+        path.join(__dirname, "../public/ssl/nonhana-server.cn_bundle.pem")
       ),
     };
     return https.createServer(options, app);
